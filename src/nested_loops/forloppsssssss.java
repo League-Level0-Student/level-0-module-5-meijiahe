1b9f268629a908f9173e460bdaaa70601e807f4f
elijahshark <league@wts-imac-11.attlocal.net>
2018-08-15 18:28:10 -0700

    forloopssssscircleringuesingPi

---
+++ src/nested_loops/forloppsssssss.java
@@ -0,0 +1,22 @@
+package nested_loops;
+
+public class forloppsssssss {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 100; i++) {
+			//System.out.println(i);
+		}
+		for (int i = 100; i >=0; i--) {
+			//System.out.println(i);
+		}
+		for (int i = 2; i <= 100; i+=2) {
+			//System.out.println(i);
+		}
+		for (int i = 1; i <= 99; i+=2) {
+			//System.out.println(i);
+		}
+		for (int i = 1; i <= 500; i++) {
+			System.out.println(i);
+			
+		}
+	}
+}
