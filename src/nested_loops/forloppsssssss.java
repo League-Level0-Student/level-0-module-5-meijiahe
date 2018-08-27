889409e72f66137b4b9d55e90f2776dd26b680dc
league <league@wts-imac-11.attlocal.net>
2018-08-27 16:56:03 -0700

    forloopsssssss

--- src/nested_loops/forloppsssssss.java
+++ src/nested_loops/forloppsssssss.java
@@ -15,8 +15,29 @@ public class forloppsssssss {
 			//System.out.println(i);
 		}
 		for (int i = 1; i <= 500; i++) {
-			System.out.println(i);
+			//System.out.println(i);
 			
 		}
+		for (int i = 0; i<=777; i+=7) {
+			//System.out.println(i);
+		}
+		for(int i=2005;i<=2018;i++) {
+			//System.out.println(i);
+		}
+		//nested for loops
+		for (int ii = 0; ii<3; ii++) {
+			
+		for (int i = 0; i<3; i++) {
+			//System.out.println(ii+" "+i);
+		}
+		}
+for (int i = 1;i<=7;i+=3) {
+	
+	for (int ii = 2;ii<=8;ii+=3) {
+		System.out.println(i+ " "+ii);
+	}
+	
+}
+
 	}
 }
