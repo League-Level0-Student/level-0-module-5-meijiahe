cb63e99f307e14759948df204752b01a2912cdf2
Isaacpants <league@wts-imac-11.attlocal.net>
2018-09-05 18:23:47 -0700

    skillpractice

--- src/nested_loops/SkillPractice.java
+++ src/nested_loops/SkillPractice.java
@@ -1,62 +1,69 @@
 package nested_loops;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class SkillPractice {
 public static void main(String[] args) {
 SkillPractice skills = new SkillPractice();
-	skills.skill1();
-	skills.skill2();
-	skills.skill3();
+	//skills.skill1();
+//skills.skill2();
+	//skills.skill3();
 	skills.skill4();
-	skills.skill5();
+	//skills.skill5();
 }
 
-void skill1() {
+//void skill1() {
 //Use pop-ups for the following.
 //Ask the user how many dimes they have
-String answer=JOptionPane.showInputDialog(null,"How many dimes you have?");
+//String answer=JOptionPane.showInputDialog(null,"How many dimes you have?");
 
 
 
 //Tell them how many cents they have (hint multiply by 10)
-String answer=JOptionPane.show
-
+//int dimes=Integer.parseInt(answer);
+//JOptionPane.showMessageDialog(null,dimes*10);
 
 
 //Ask the user how tall they are (inches)
+//String answer1=JOptionPane.showInputDialog(null,"How tall you are");
 
-
-
+//int inches=Integer.parseInt(answer1);
 
 //If they are shorter than 36 inches, tell them to eat their Wheaties
+//if(inches<36) {
+	//System.out.println("eat your wheaties!");
+//}
+
+
+
+//}
+
+//void skill2() { // Write a loop to print every third number between 1 and 30 to the console 
+	//for (int i = 1; i <= 30; i += 3) {
+		//System.out.println(i);
+	//}
 
 
 
 
-}
-
-void skill2() { // Write a loop to print every third number between 1 and 30 to the console 
 
 
-
-
-
-
-
-}
+//}
 
 void skill3() { // Get a random number that is less than 20 and print it to the console 
-
-
-
+	Random gen= new Random();
+int number=gen.nextInt(20);
+System.out.println(number);
 //Get another random number that is less than 10 and print it to the console 
-
+int number1=gen.nextInt(10);
+System.out.println(number1);
 
 
 //Using a pop-up, tell the user the difference between the numbers // Hint: use subtraction 
 
-
+JOptionPane.showMessageDialog(null,"Differences between number and number1 is "+(number-number1));
 
 }
 
